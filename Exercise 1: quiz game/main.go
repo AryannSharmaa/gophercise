05a84d6b0bf18e1d55e7a136329d050fa17e38f4
Aryan <aryancr72000@gmail.com>
2024-04-14 23:48:50 +0530

    completed first part

--- Exercise 1: quiz game/main.go
+++ Exercise 1: quiz game/main.go
@@ -4,21 +4,36 @@ import (
 	"encoding/csv"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
-	fmt.Println("hi")
 	file, error := os.Open("problems.csv")
 	if error != nil {
 		fmt.Println(error)
 	}
-	fmt.Println("file opened")
 	defer file.Close()
-
 	filereader := csv.NewReader(file)
 	rec, error := filereader.ReadAll()
 	if error != nil {
 		fmt.Println(error)
 	}
-	fmt.Println(rec)
+
+	correct, incorrect := 0, 0
+	fmt.Println("Write correct answers")
+	for _, v := range rec {
+		result, error := (strconv.Atoi(v[1]))
+		if error != nil {
+			fmt.Println(error)
+		}
+		fmt.Print(v[0], "=")
+		var answer int
+		fmt.Scanln(&answer)
+		if answer == result {
+			correct++
+		} else {
+			incorrect++
+		}
+	}
+	fmt.Printf("Total: %v correct: %v, incorrect: %v\n", correct+incorrect, correct, incorrect)
 }
